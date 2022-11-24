be8389c0fbc176027210d03277d993e79255b5c5
sinha45 <alfa45leonidas@gmail.com>
2022-11-24 12:04:37 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './Layout/Main';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/Blog',
+          element: <Blog></Blog>
+        }
+      ]
+    }
+  ])
+
   return (
     <div className="App">
-      <h1 className='text-4xl font-bold'>Tailwind added</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
+        <div className='bg-blue-400 flex h-14 shadow-xl'>
+            <div className='ml-72 m-auto'>
+                <h1 className='font-bold text-3xl text-white'>Quiz-<span className='text-violet-600'>Busters</span></h1>
+            </div>
+            <div className='flex-auto m-auto text-white font-semibold space-x-14'>
+                <Link to='/'>Home</Link>
+                <Link to='/topics'>Topics</Link>
+                <Link to='/statistics'>Statistics</Link>
+                <Link to='/blog'>Blog</Link>
 
+            </div>
         </div>
     );
 };
