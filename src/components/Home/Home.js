0d98bad509de79d5d0d23e94fb8ce111c8fc92f5
sinha45 <alfa45leonidas@gmail.com>
2022-11-24 12:32:25 +0600

    home added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div className='container flex'>
+            <div className='flex-1 ml-72'>
+                <img src="https://d1ymz67w5raq8g.cloudfront.net/Pictures/480xany/6/5/5/509655_shutterstock_1506580442_769367.jpg" alt="" />
+            </div>
+            <div className='bg-cyan-400 w-72 flex-1'>
+                <p className='text-2xl text-white mt-4 p-5'>
+                    This quiz website will make your brain strong. you will know more about programming languages. This website is the easy learning website. You can increase your knowledge.
+                </p>
+            </div>
         </div>
     );
 };
