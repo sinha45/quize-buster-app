8d843133d62524290a873d9e6d3a7d498325786b
sinha45 <alfa45leonidas@gmail.com>
2022-11-24 10:53:45 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-4xl font-bold'>Tailwind added</h1>
+    </div>
+  );
+}
+
+export default App;
